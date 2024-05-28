54bca01b7254ec41b4c46d552a851a5b284f88ad
Miguel Augusto <miguelmasche@gmail.com>
2024-05-28 16:45:59 -0700

    add game and question files

--- game.rb
+++ game.rb
@@ -1,2 +1,16 @@
-class game
+    # game.rb
+require_relative "player"
+require_relative "question"
+
+class Game
+    def initialize
+        @player1 = Player.new("Player 1")
+        @player2 = Player.new("Player 2")
+        @current_player = @player1
+    end
+
+    # the start_game method will contain the main game loop
+    def start_game
+
+    end
 end
--- question.rb
+++ question.rb
@@ -1,3 +1,17 @@
+# question.rb
 
 class Question
+    attr_reader :number1, :number2
+
+    # initialize and generate 2 random numbers, then assign them to instance variables
+    def initialize
+        @number1 = rand(1..20)
+        @number2 = rand(1..20)
+    end
+end
+
+    # correct? method will check to see if the provided answer matches the sum of the two nums
+    def correct?
+        answer == @number1 + @number2
+    end
 end
